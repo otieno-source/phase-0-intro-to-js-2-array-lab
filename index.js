cbd4196b65249cc05a9368d7a7bea2627478cec6
collins ouma otieno <otiencollins0@gmail.com>
2024-07-05 12:23:17 +0300

    INTRO TO JS-ARRAY LAB

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(Broom) {
+    return [...cats, "Broom"];
+}
+
+function prependCat(Arnold) {
+    return ['Arnold', ...cats];
+}
+
+function removeLastCat(){
+    let newcat = cats.slice(0, -1);
+    return newcat;
+}
+
+function removeFirstCat(){
+    let newcat = cats.slice(1);
+    return newcat;
+}
